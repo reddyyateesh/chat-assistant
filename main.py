dfacbb39bbffa03fdda2e6664c87152f8f63f1fd
Yateesh Reddy <reddyyateesh999@gmail.com>
2024-07-06 20:11:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ console = Console()
 API_KEY = "..." # You can get it from https://console.groq.com/keys
 
 def generate_groq_prompt(prompt: str, model: str="llama3-70b-8192") -> Optional[str]:
-    error_message = (
-        "Sorry, I'm unable to assist with that particular request."
-        "If there's anything else you need help with, please feel free to let me know!"
-    )
     try:
         url = "https://api.groq.com/openai/v1/chat/completions"
 
@@ -41,20 +37,28 @@ def generate_groq_prompt(prompt: str, model: str="llama3-70b-8192") -> Optional[
             response = response_data['choices'][0]['message']['content']
 
             return response
-
+        else:
+            return
     except:
-        console.print(error_message)
+        return
 
-def process_user_input() -> None:
-    prompt = input()
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K') 
-    console.print(f"User: {prompt}", style='yellow')
-    response = generate_groq_prompt(prompt)
-
-    console.print(Markdown(f"Assistant: {response}"), style='cyan')
 
 console.print('Assistant: Hello and welcome.! How can I assist you today?', style='cyan')
 
-while True:
-    process_user_input()
+@lambda _: _()
+def process_user_input() -> None:
+    error_message = (
+        "Sorry, I'm unable to assist with that particular request. "
+        "If there's anything else you need help with, please feel free to let me know!"
+    )
+    while True:
+        prompt = input()
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K') 
+        console.print(f"User: {prompt}", style='yellow')
+        response = generate_groq_prompt(prompt)
+
+        if response is not None:
+            console.print(Markdown(f"Assistant: {response}"), style='cyan')
+        else:
+            console.print(error_message, style='cyan')
